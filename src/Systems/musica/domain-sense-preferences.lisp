816819f5a3bfa02ebbaa766012be2eb66e881fba
William de Beaumont <wdebeaum@cs.rochester.edu>
2017-04-23 01:42:43 -0400

    ../update-git-mirror-of-cvs-module.rb

--- src/Systems/musica/domain-sense-preferences.lisp
+++ src/Systems/musica/domain-sense-preferences.lisp
@@ -4,4 +4,6 @@
 (setf om::*domain-sense-preferences* '(
 				       (W::step ONT::whole-step)
 				       (W::NOTE ONT::NOTE-PITCH)
+				       (W::BAR ONT::BAR-MEASURE)
+				       (W::MEASURE ONT::BAR-MEASURE)
 				       ))
